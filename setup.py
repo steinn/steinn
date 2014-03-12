ab6605c71216a546fdca732692b6ef6698384412
Tómas Árni Jónasson <tomas@clara.is>
2014-03-12 11:50:46 -0700

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='steinn',
+      version='1.0',
+      description='The Steinn module',
+      author='Tómas Árni Jónasson and Magnús Ingi Sveinbjörnsson',
+      author_email='mail+steinn@tomas.arni.jonasson.name',
+      py_modules = ['steinn'],
+     )
+
